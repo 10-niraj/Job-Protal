9546abb39d6f655c1c886e703c346187d61c06c2
Niraj <your-email@example.com>
2026-09-12 11:55:00 +0530

    fixapi url to live backend

--- frontend/src/components/Jobs.jsx
+++ frontend/src/components/Jobs.jsx
@@ -1,6 +1,6 @@
 import { useEffect, useState } from "react";
 
-const API_URL = "http://127.0.0.1:5000/api/jobs";
+const API_URL = "https://demo-job-protal-backend.vercel.app/api/jobs";
 
 function Jobs() {
   const [jobs, setJobs] = useState([]);
